0a1395b2e3982f3b7abeffc5552896fa0fdf2468
Jaydevloper <jayhanmirx@gmail.com>
2024-04-13 11:07:03 +0500

    edition:eslintr and useGet

--- src/hooks/useGet/index.tsx
+++ src/hooks/useGet/index.tsx
@@ -4,22 +4,21 @@ import api from "services/api";
 interface IProps {
   url: string;
   name: string;
-  params?: null;
   onError?: () => void;
   onSuccess?: () => void;
   queryOptions?: UseQueryOptions<any, any, any, any>;
 }
 
-async function GetData({ url = "", params = null }) {
+async function GetData({ url = "" }) {
   const res = await api.get(url);
   return res.data;
 }
 
 function useGet(props: IProps): UseQueryResult {
-  const { name, url, params, onError, onSuccess, queryOptions } = props;
+  const { name, url, onError, onSuccess, queryOptions } = props;
   const data = useQuery({
-    queryKey: [`${name}`, { url, params }],
-    queryFn: () => GetData({ url, params }),
+    queryKey: [`${name}`, { url }],
+    queryFn: () => GetData({ url }),
     onError: onError,
     onSuccess: onSuccess,
     ...queryOptions,
