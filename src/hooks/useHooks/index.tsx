d8545346a6e58fc1413e7ec04750ad222fa41db8
Jaydevloper <jayhanmirx@gmail.com>
2024-04-17 18:32:44 +0500

    edition:install "lodash" and useHooks

--- src/hooks/useHooks/index.tsx
+++ src/hooks/useHooks/index.tsx
@@ -1,13 +1,15 @@
 import { useLocation, useNavigate, useParams } from "react-router-dom";
 import qs from "qs";
+import lodash from "lodash";
+import { useQueryClient } from "react-query";
 const useHooks = () => {
+  const queryClieant = useQueryClient();
   const params = useParams();
   const navigate = useNavigate();
   const location = useLocation();
+  const query = qs.parse(location.search, { ignoreQueryPrefix: true });
 
-  const query = qs.parse(location.search);
-
-  return { params, navigate };
+  return { params, navigate, queryClieant, query, location, qs, ...lodash };
 };
 
 export default useHooks;
