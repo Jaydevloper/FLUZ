d1191891c41961cce21f84279909f581fe50b966
Jaydevloper <jayhanmirx@gmail.com>
2024-04-22 17:10:43 +0500

    edition:CD yml and routes file

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,8 +3,24 @@ import { createBrowserRouter } from "react-router-dom";
 const router = () => {
   return createBrowserRouter([
     {
+      path: "/sign-up",
+      element: <div>SignUp</div>,
+    },
+    {
+      path: "/sign-in",
+      element: <div>SignUp</div>,
+    },
+    {
       path: "/",
-      element: <></>,
+      errorElement: <div>Error</div>,
+      element: <div>Header</div>,
+      loader: () => <div>Loading....</div>,
+      children: [
+        {
+          index: true,
+          element: <div>Home</div>,
+        },
+      ],
     },
   ]);
 };
